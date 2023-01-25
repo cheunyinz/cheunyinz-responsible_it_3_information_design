03836fcd4a9b4ce9c8f8fea9a6422a9a6a3130de
cheunyinz <cheunyinzhang@gmail.com>
2023-01-25 14:33:53 +0100

    fix sunburst and netlify

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+export default defineConfig({
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                fetch: resolve(__dirname, 'fetch.html'),
+                sunburst: resolve(__dirname, 'sunburst.html'),
+
+            }
+        }
+    },
+});
