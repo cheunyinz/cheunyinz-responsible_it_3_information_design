14cf315ae95f307d990620568e5de036edff8d84
Whitneyas <whitneyserwaa@gmail.com>
2023-01-25 13:03:49 +0100

    add sunburst / frontend

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,10 @@ import "../styles/main.scss";
 // We can use node_modules directely in the browser!
 import * as d3 from 'd3';
 
+
 console.log('Hello, world!');
 
 let data = count(5, 5);
 
 console.log(data);
+
